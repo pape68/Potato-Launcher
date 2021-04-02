682b0695101450b91846241aa9000623268579b9
Potato <77371158+a-bakedpotato@users.noreply.github.com>
2021-04-02 00:34:44 -0700

    Delete browserLink.js

--- assets/script/browserLink.js
+++
@@ -1,7 +0,0 @@
-const electron = require('electron');
-
-document.body.addEventListener('click', e => {
-    if (e.target.tagName.toLowerCase() !== 'a') return null;
-    e.preventDefault();
-    electron.shell.openExternal(e.target.href).then();
-});
