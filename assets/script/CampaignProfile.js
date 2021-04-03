b52172bc4b5771d39fb32b04e12046706c55a8fe
Potato <z4bakedpotato@gmail.com>
2021-04-03 00:00:46 -0700

    finish stw profile

--- assets/script/CampaignProfile.js
+++ assets/script/CampaignProfile.js
@@ -1,4 +1,4 @@
-import { axios, iosBasic } from './requests.js';
+import { axios } from './requests.js';
 import { VerifiedToken } from "./VerifiedToken.js";
 
 const fs = require('fs');
@@ -6,7 +6,7 @@ const accounts = fs.existsSync(process.env.appdata+'/a.bakedpotato/fnappv2/accou
     JSON.parse(fs.readFileSync(process.env.appdata+'/a.bakedpotato/fnappv2/accounts.json').toString()) :
     [];
 
-export class CampaignProfile {
+export class ExtendedCampaignProfile {
     constructor(accountId, onFinish) {
         new VerifiedToken(null, async token => {
             if (token.length !== 32) return onFinish(token);
@@ -32,6 +32,14 @@ export class CampaignProfile {
             if (!acc[0]) return onFinish(acc.errorMessage);
             acc = acc[0];
 
+            let commonPublic = await axios.post('https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/'+acc.id+'/public/QueryPublicProfile?profileId=common_public',
+                '{}',
+                {
+                    'Authorization': 'bearer '+token,
+                    'Content-Type': 'application/json'
+                }
+            );
+
             let campaign = await axios.post('https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/'+acc.id+'/public/QueryPublicProfile?profileId=campaign',
                 '{}',
                 {
@@ -40,8 +48,17 @@ export class CampaignProfile {
                 }
             );
             if (campaign.errorMessage) return onFinish(campaign.errorMessage);
-            campaign = campaign.profileChanges[0].profile.items;
+            campaign = campaign.profileChanges[0].profile;
+            campaign.homebase_name = commonPublic.profileChanges[0].profile.stats.attributes.homebase_name;
             onFinish(campaign, acc);
+        });
+    }
+}
+
+export class CampaignProfile {
+    constructor(accountId, onFinish){
+        new ExtendedCampaignProfile(accountId,  profile => {
+            onFinish(profile.items);
         });
     }
 }
--- pages/stwProfile/stwProfile.js
+++ pages/stwProfile/stwProfile.js
@@ -1,4 +1,4 @@
-import { CampaignProfile } from '../../assets/script/CampaignProfile.js';
+import { ExtendedCampaignProfile } from '../../assets/script/CampaignProfile.js';
 import { formatNum } from '../../assets/script/util.js';
 
 const fs = require('fs');
@@ -13,7 +13,7 @@ function outputText(text){
 
 async function lookupAcc(){
     let input = document.getElementById('input').value;
-    new CampaignProfile(input, (profile, acc) => {
+    new ExtendedCampaignProfile(input, (profile, acc) => {
         clearOutput();
         if (typeof profile === 'string') {
             return outputText(profile);
@@ -22,44 +22,72 @@ async function lookupAcc(){
         let table = document.createElement('table');
         let addRow = (col1, col2) => table.innerHTML += '<tr><td><b>'+col1+'</b></td><td>'+col2+'</td></tr>';
         let addBlankRow = () => table.innerHTML += '<tr><td height="10" colspan="2"></td></tr>';
-        let addTitleRow = (col1, col2) => addRow(col1, '<b>'+col2+'</b>');
+        let addTitleRow = (col1, col2) => addRow('<u>'+col1+'</u>', '<b><u>'+col2+'</u></b>');
+
+        addTitleRow('Save the World', 'Profile');
+        addRow('Commander Level:', formatNum(profile.stats.attributes.level+profile.stats.attributes.rewards_claimed_post_max_level));
+        addRow('Days Logged In:', formatNum(profile.stats.attributes.daily_rewards.totalDaysLoggedIn || 0));
+        addRow('Mythic Schematics:', Object.keys(profile.items).filter(key => profile.items[key].templateId.includes('stormking_sr')).length);
+        addRow('Homebase Name:', profile.homebase_name);
+        addRow('Revisions:', formatNum(profile.rvn));
+        addBlankRow();
 
         addTitleRow('Banner', 'Challenges');
-        let names = {
-            buildstructures: 'Talented Builder',
-            destroygnomes: 'Go Gnome!',
-            explorezones: 'World Explorer',
-            killmistmonsters: 'Unspeakable Horrors',
-            loottreasurechests: 'Loot Legend',
-            playwithothers: 'Plays Well with Others',
-            savesurvivors: 'Guardian Angel'
+        let data = {
+            buildstructures: {
+                name: 'Talented Builder',
+                value: 'build_any_structure',
+                amt: 500000
+            },
+            destroygnomes: { name: 'Go Gnome!', value: 'destroy_gnome', amt: 100 },
+            explorezones: {
+                name: 'World Explorer',
+                value: 'complete_exploration_1',
+                amt: 1500
+            },
+            killmistmonsters: {
+                name: 'Unspeakable Horrors',
+                value: 'kill_husk_smasher',
+                amt: 20000
+            },
+            loottreasurechests: { name: 'Loot Legend', value: 'interact_treasurechest', amt: 300 },
+            playwithothers: {
+                name: 'Plays Well with Others',
+                value: 'quick_complete',
+                amt: 1000
+            },
+            savesurvivors: {
+                name: 'Guardian Angel',
+                value: 'questcollect_survivoritemdata',
+                amt: 10000
+            }
         }
-        let values = {
-            buildstructures: 'build_any_structure',
-            destroygnomes: 'destroy_gnome',
-            explorezones: 'complete_exploration_1',
-            killmistmonsters: 'kill_husk_smasher',
-            loottreasurechests: 'interact_treasurechest',
-            playwithothers: 'quick_complete',
-            savesurvivors: 'questcollect_survivoritemdata'
+        for (const value of Object.keys(profile.items).filter(key => profile.items[key].templateId.startsWith('Quest:achievement_')).map(key => profile.items[key])){
+            let key = value.templateId.split('_')[1];
+            let info = data[key];
+            if (!info) continue;
+            let done = value.attributes['completion_'+info.value];
+            addRow('<img src="../../assets/img/banners/'+info.name.toLowerCase().replace(/\W/g, '')+'.png" width="6%" alt="banner"> '+info.name+':', formatNum(done)+'/'+formatNum(info.amt)+' ('+((done/info.amt)*100).toString().substr(0, 5)+'%)');
         }
-        let amts = {
-            buildstructures: 500000,
-            destroygnomes: 100,
-            explorezones: 1500,
-            killmistmonsters: 20000,
-            loottreasurechests: 300,
-            playwithothers: 1000,
-            savesurvivors: 10000
+        addBlankRow();
+
+        addTitleRow('Research', 'Levels');
+        for (const item of ['Fortitude', 'Offense', 'Resistance', 'Technology']){
+            addRow('<img src="../../assets/img/fort/'+item.toLowerCase()+'.png" width="6%" alt="'+item+'"> '+item+':', profile.stats.attributes.research_levels[item.toLowerCase()]);
         }
-        for (const value of Object.keys(profile).filter(key => profile[key].templateId.startsWith('Quest:achievement_')).map(key => profile[key])){
-            let name = names[value.templateId.split('_')[1]];
-            let progress = values[value.templateId.split('_')[1]];
-            let amt = amts[value.templateId.split('_')[1]];
-            let done = value.attributes['completion_'+progress];
-            if (!name) continue;
-            addRow('<img src="../../assets/img/banners/'+name.toLowerCase().replace(/\W/g, '')+'.png" width="6%" alt="banner"> '+name+':', formatNum(done)+'/'+formatNum(amt)+' ('+((done/amt)*100).toString().substr(0, 5)+'%)');
-        }
+        let research = profile.items[Object.keys(profile.items).filter(key => profile.items[key].templateId === 'Token:collectionresource_nodegatetoken01')[0]].quantity;
+        addRow('<img src="../../assets/img/fort/research.png" width="6%" alt="research"> Research Points:', formatNum(research));
+        addBlankRow();
+
+        addTitleRow('Collection', 'Book');
+        addRow('Level:', formatNum(profile.stats.attributes.collection_book.maxBookXpLevelAchieved+1));
+        addRow('Unslot Cost:', '<img src="../../assets/img/vbucks.png" width="8%" alt="vkeks" class="col2"> '+formatNum(profile.stats.attributes.unslot_mtx_spend));
+        addBlankRow();
+
+        addTitleRow('Profile', 'Dates');
+        addRow('Creation Date:', profile.created.split('T')[0]);
+        addRow('Last Updated:', profile.updated.split('T')[0]);
+        addBlankRow();
 
         outputText('<h1>'+acc.displayName+'</h1>');
         document.getElementById('output').appendChild(table);
