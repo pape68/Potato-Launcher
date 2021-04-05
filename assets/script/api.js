2c9125692436d725acabd756e1d5f0d37a190db1
Potato <z4bakedpotato@gmail.com>
2021-04-04 22:45:09 -0700

    research

--- assets/script/api.js
+++ assets/script/api.js
@@ -1,4 +1,5 @@
 import { axios } from './requests.js';
+import { CampaignProfile } from './CampaignProfile.js';
 import { VerifiedTokenPromise } from './VerifiedToken.js';
 
 function composeMcp(accountId, endpoint, profile, payload){
@@ -17,7 +18,17 @@ function composeMcp(accountId, endpoint, profile, payload){
 }
 
 export const api = {
+    ClaimCollectedResources: (accountId) => {
+        return new Promise(resolve => {
+            new CampaignProfile(accountId, async profile => {
+                if (profile.errorMessage) return resolve(profile.errorMessage);
+                let collectorsToClaim = Object.keys(profile).filter(key => profile[key].templateId === 'CollectedResource:Token_collectionresource_nodegatetoken01');
+                resolve(await composeMcp(accountId, 'ClaimCollectedResources', 'campaign', '{"collectorsToClaim":'+JSON.stringify(collectorsToClaim)+'}'));
+            });
+        });
+    },
     ClaimLoginReward: (accountId) => { return composeMcp(accountId, 'ClaimLoginReward', 'campaign') },
+    PurchaseResearchStatUpgrade: (accountId, stat) => { return composeMcp(accountId, 'PurchaseResearchStatUpgrade', 'campaign', '{"statId":"'+stat+'"}') },
     QueryProfile: (accountId, profile) => { return composeMcp(accountId, 'QueryProfile', profile) },
     SetHomebaseName: (accountId, homebaseName) => { return composeMcp(accountId, 'SetHomebaseName', 'common_public', '{"homebaseName":"'+homebaseName+'"}') }
 }
---
+++ pages/research/research.js
@@ -0,0 +1,101 @@
+import { api } from '../../assets/script/api.js';
+import { formatNum } from '../../assets/script/util.js';
+import { ExtendedCampaignProfile } from '../../assets/script/CampaignProfile.js';
+
+const fs = require('fs');
+const costTable = JSON.parse(fs.readFileSync(__dirname+'/../../assets/json/ResearchSystem.json').toString());
+
+const accounts = JSON.parse(fs.readFileSync(process.env.appdata + '/a.bakedpotato/fnappv2/accounts.json').toString());
+
+const select = document.getElementById('accounts');
+for (const account of accounts) {
+    let acc = document.createElement('option');
+    acc.textContent = account.displayName;
+    acc.value = account.accountId;
+    select.appendChild(acc);
+}
+
+function clearOutput() {
+    document.getElementById('output').innerHTML = '';
+}
+
+function outputText(text) {
+    document.getElementById('output').innerHTML += text + '<br>';
+}
+
+async function research() {
+    let input = document.getElementById('accounts').value;
+    let acc = accounts.filter(acc => acc.accountId === input)[0];
+    clearOutput();
+    outputText('Loading <img src="../../assets/img/loading.gif" alt="loading" class="loading" width="4%">');
+
+    api.ClaimCollectedResources(acc.accountId).then();
+    new ExtendedCampaignProfile(acc.accountId, profile => {
+        if (profile.errorMessage){
+            clearOutput();
+            return outputText(profile.errorMessage);
+        }
+
+        let table = document.createElement('table');
+        function addRow(col1, col2, col3){
+            let row = document.createElement('tr');
+            row.innerHTML = '<td><b>'+col1+'</b></td><td>'+col2+'</td><td>'+(col3 || '<img src="../../assets/img/blank.png" width="5%" alt="upgrade">')+'</td>';
+            table.appendChild(row);
+        }
+
+        let researchPoints = profile.items[
+            Object.keys(profile.items)
+                .filter(item => profile.items[item].templateId === 'Token:collectionresource_nodegatetoken01')[0]
+            ].quantity;
+        clearOutput();
+        outputText('<h2><img src="../../assets/img/fort/research.png" width="4%" alt="research"> '+formatNum(researchPoints)+'</h2>');
+
+        let max = 0;
+        for (const stat of ['Fortitude', 'Offense', 'Resistance', 'Technology']){
+            let level = profile.stats.attributes.research_levels[stat.toLowerCase()];
+            let cost = 0;
+
+            for (const item of costTable[0].ExportValue[stat.toLowerCase()+'_cost'].Keys){
+                if (item.KeyTime <= level) cost = item.KeyValue;
+            }
+
+            if (level < 120){
+                if (cost <= researchPoints){
+                    addRow(
+                        '<img src="../../assets/img/fort/'+stat.toLowerCase()+'.png" width="4%" class="col1" alt="'+stat+'"> '+stat,
+                        level,
+                        '<img src="../../assets/img/up_arrow.png" style="cursor: pointer" width="5%" onclick="upgrade(\''+acc.accountId+'\', \''+stat.toLowerCase()+'\')" alt="upgrade"> <img src="../../assets/img/fort/research.png" width="4%" alt="research"> '+formatNum(cost)
+                    );
+                } else {
+                    addRow(
+                        '<img src="../../assets/img/fort/'+stat.toLowerCase()+'.png" width="4%" class="col1" alt="'+stat+'"> '+stat,
+                        level,
+                        '<img src="../../assets/img/x.png" width="5%" alt="upgrade"> <img src="../../assets/img/fort/research.png" width="4%" alt="research"> '+formatNum(cost)
+                    )
+                }
+            } else {
+                max++;
+                addRow(
+                    '<img src="../../assets/img/fort/'+stat.toLowerCase()+'.png" width="4%" class="col1" alt="'+stat+'"> '+stat,
+                    level
+                );
+            }
+        }
+        console.log(max);
+        if (max === 4) table.classList.add('max');
+
+        document.getElementById('output').appendChild(table);
+    });
+}
+
+async function upgrade(accountId, stat){
+    let upgradePoint = await api.PurchaseResearchStatUpgrade(accountId, stat);
+    if (upgradePoint.errorMessage){
+        clearOutput();
+        return outputText(upgradePoint.errorMessage);
+    }
+    research().then();
+}
+
+window.research = research;
+window.upgrade = upgrade;
